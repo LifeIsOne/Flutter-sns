7ddb9c1c569c8245b09d356b440dbfc55c28158b
LifeIsOne <didtmdgh258@gmail.com>
2024-04-17 12:07:10 +0900

    하단 바, 스크린 구성

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({
@@ -21,6 +22,22 @@ class _MainScreenState extends State<MainScreen> {
           Center(child: Text("첫번째 스크린입니다.")),
           Center(child: Text("두번째 스크린입니다.")),
           Center(child: Text("세번째 스크린입니다."))
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.user),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comment),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.ellipsisH),
+            label: "",
+          ),
         ],
       ),
     );
