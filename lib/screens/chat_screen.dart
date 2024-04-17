2b590101a79fa4db495aee5ce888165e5448496d
LifeIsOne <didtmdgh258@gmail.com>
2024-04-17 19:55:53 +0900

    채팅 목록보기 구현

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kakao/models/chat.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({
@@ -7,8 +8,82 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("두번째 페이지입니다."),
+    return Scaffold(
+      // ✅(목록보기)채팅방 배경색
+      backgroundColor: Colors.white,
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ChatCard(chat: chats[index]);
+        },
+      ),
+    );
+  }
+}
+
+class ChatCard extends StatelessWidget {
+  const ChatCard({
+    super.key,
+    required this.chat,
+  });
+
+  final Chat chat;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(chat.image),
+                ),
+                title: Text(
+                  chat.title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+                subtitle: Text(chat.content),
+                contentPadding: const EdgeInsets.all(0),
+              ),
+            ),
+            // ✅톡 온 시간 표시
+            Column(
+              children: [
+                Text(
+                  chat.time,
+                  style: TextStyle(
+                    color: Color(0xFFA5A5A5),
+                    fontSize: 12,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                if (chat.count != "0")
+                  Container(
+                    // ✅padding으로 안읽은 메시지 숫자를 동그랗게? 왜?
+                    padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Color(0xFFDE6344),
+                    ),
+                    child: Text(
+                      chat.count,
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
