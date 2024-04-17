e266a1eb20844075874be07d86257108e8808cee
LifeIsOne <didtmdgh258@gmail.com>
2024-04-17 12:16:46 +0900

    BottomNavigationBar를 이용한 탭 전환

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -25,6 +25,22 @@ class _MainScreenState extends State<MainScreen> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        // 클릭했을 때/안 돼있을 때 올라옴
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        // 해당탭의 인댁스 지정
+        currentIndex: _selIdx,
+        // 하단 바(백그라운드) 색상
+        backgroundColor: Colors.grey[50],
+        // 선택된 아이콘 색상
+        selectedItemColor: Colors.black,
+        // 선택되지 않은 아이콘 색상
+        unselectedItemColor: Colors.black54,
+        onTap: (i) {
+          setState(() {
+            _selIdx = i;
+          });
+        },
         items: [
           BottomNavigationBarItem(
             icon: Icon(FontAwesomeIcons.user),
