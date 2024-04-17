5d81aa36a57b8bba094be1062d21c08bedc9dec4
LifeIsOne <didtmdgh258@gmail.com>
2024-04-17 12:49:06 +0900

    ProfileCard 컴포넌트 생성

--- lib/screens/friend_screen.dart
+++ lib/screens/friend_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kakao/models/user.dart';
+
+import '../components/profile_card.dart';
 
 class FriendScreen extends StatelessWidget {
   const FriendScreen({
@@ -10,6 +13,13 @@ class FriendScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(title: Text("친구")),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          //   프로필 - 재사용
+          ProfiledCard(user: me)
+        ],
+      ),
     );
   }
 }
