6603a518171876701ce347faf2993ad89dd9a9dc
LifeIsOne <didtmdgh258@gmail.com>
2024-04-18 12:26:56 +0900

    더보기 아이콘 배치

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/tab.dart';
+
 class MoreScreen extends StatelessWidget {
   const MoreScreen({
     super.key,
@@ -8,16 +10,26 @@ class MoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.lightGreenAccent,
       appBar: AppBar(
         title: const Text("더보기"),
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 30),
         child: GridView.count(
-          crossAxisCount: 4,
-          children: [],
-        ),
+            crossAxisCount: 4,
+            children: List.generate(
+                tabs.length,
+                (index) => Column(
+                      children: [
+                        Icon(tabs[index].icon) /* 아이콘 정렬*/,
+                        SizedBox(height: 5) /* 아이콘과 텍스트 사이*/,
+                        Text(
+                          tabs[index].text /* 텍스트*/,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ))),
       ),
     );
   }
